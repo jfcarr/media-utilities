f2d8afb0466fa0e549b0bbe67e36bd2c88e7f8d5
jfcarr <jfcarr@gmail.com>
2017-04-29 21:19:53 -0400

    cleanup

--- rokuconvert.py
+++ rokuconvert.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 
-# Example call: HandBrakeCLI -Z "Very Fast 1080p30" -i "deer.wmv" -o "deer.mp4"
-
 import argparse
 import glob
 import os
 import sys
 
-supported_extensions = ['.avi','.mkv','.webm','.wmv']
+_converterName = 'HandBrakeCLI'
+_converterPreset = ' -Z "Very Fast 1080p30" '
+_supported_extensions = ['.avi','.mkv','.webm','.wmv']
 
-def ConvertFile(filename):
+def ConvertFile(input_file):
 	if not os.path.isfile(input_file):
 		raise ValueError(input_file + " not found.")
 
 	input_file_name, input_file_extension = os.path.splitext(input_file)
 	output_file = input_file_name + ".mp4"
 
-	if any(x in input_file_extension for x in supported_extensions):
-		full_command = "HandBrakeCLI -Z \"Very Fast 1080p30\" -i \"" + input_file + "\" -o \"" + output_file + "\""
+	if any(x in input_file_extension for x in _supported_extensions):
+		full_command = _converterName + _converterPreset + "-i \"" + input_file + "\" -o \"" + output_file + "\""
 		
 		os.system(full_command)
 	else:
@@ -27,41 +27,38 @@ def GenerateScript():
 	fp1 = open('rconvert.sh', 'w')
 	print("#!/bin/bash", file=fp1)
 	print("", file=fp1)
-	for extension in supported_extensions:
+	for extension in _supported_extensions:
 		filenames = glob.glob('*' + extension)
 		for filename in filenames:
 			input_file_name, input_file_extension = os.path.splitext(filename)
 			output_file = input_file_name + ".mp4"
-			execCommand = "HandBrakeCLI -Z \"Very Fast 1080p30\" -i \"" + filename + "\" -o \"" + output_file + "\""
+			execCommand = _converterName + _converterPreset + "-i \"" + filename + "\" -o \"" + output_file + "\""
 			print(execCommand, file=fp1)
 	fp1.close()
 	os.system("chmod u+x rconvert.sh")
-	sys.exit(0)
 
-parser = argparse.ArgumentParser()
 
-try:
-	parser.add_argument("-f", "--file", type=str, help="name of file to convert")
-	parser.add_argument("-s", "--script", help="generate conversion script", action="store_true")
-	args = parser.parse_args()
-except Exception as ex:
-	print(ex)
-	sys.exit(1)
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
 
-if args.script:
-	GenerateScript()
+	try:
+		parser.add_argument("-f", "--file", type=str, help="name of file to convert")
+		parser.add_argument("-s", "--script", help="generate conversion script", action="store_true")
+		args = parser.parse_args()
+	except Exception as ex:
+		print(ex)
+		sys.exit(1)
 
-input_file = ""
+	try:
+		if args.script:
+			GenerateScript()
+			sys.exit(0)
 
-try:
-	if not args.file:
-		raise ValueError("You must specify a file to convert.")
-	else:
-		input_file = args.file
+		if args.file:
+			ConvertFile(args.file)
+			sys.exit(0)
+	except Exception as ex:
+		print(ex)
+		sys.exit(1)
 
-	ConvertFile(input_file)
-		
-except Exception as ex:
-	print(ex)
-	sys.exit(1)		
-
+	print("Not sure what you want to do...")
