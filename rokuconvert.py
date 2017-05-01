1b56b874123eb1f25257ecf0fdbaeb200db47091
jfcarr <jfcarr@gmail.com>
2017-04-30 22:10:37 -0400

    Refactored individual functions into class.

--- rokuconvert.py
+++ rokuconvert.py
@@ -5,38 +5,39 @@ import glob
 import os
 import sys
 
-_converterName = 'HandBrakeCLI'
-_converterPreset = ' -Z "Very Fast 1080p30" '
-_supported_extensions = ['.avi','.mkv','.mov','.webm','.wmv']
+class CRoku:
+	def __init__(self, _converterName, _converterPreset, _supported_extensions):
+		self.ConverterName = _converterName
+		self.ConverterPreset = _converterPreset
+		self.SupportedExtensions = _supported_extensions
 
-def ConvertFile(input_file):
-	if not os.path.isfile(input_file):
-		raise ValueError(input_file + " not found.")
+	def ConvertFile(self, input_file):
+		if not os.path.isfile(input_file):
+			raise ValueError(input_file + " not found.")
 
-	input_file_name, input_file_extension = os.path.splitext(input_file)
-	output_file = input_file_name + ".mp4"
+		input_file_name, input_file_extension = os.path.splitext(input_file)
+		output_file = input_file_name + ".mp4"
 
-	if any(x in input_file_extension for x in _supported_extensions):
-		full_command = _converterName + _converterPreset + "-i \"" + input_file + "\" -o \"" + output_file + "\""
-		
-		os.system(full_command)
-	else:
-		raise ValueError(input_file + " is not in a format that can be converted.")	
+		if any(x in input_file_extension for x in self.SupportedExtensions):
+			full_command = self.ConverterName + " " + self.ConverterPreset + " -i \"" + input_file + "\" -o \"" + output_file + "\""
+			
+			os.system(full_command)
+		else:
+			raise ValueError(input_file + " is not in a format that can be converted.")	
 
-def GenerateScript():
-	fp1 = open('rconvert.sh', 'w')
-	print("#!/bin/bash", file=fp1)
-	print("", file=fp1)
-	for extension in _supported_extensions:
-		filenames = glob.glob('*' + extension)
-		for filename in filenames:
-			input_file_name, input_file_extension = os.path.splitext(filename)
-			output_file = input_file_name + ".mp4"
-			execCommand = _converterName + _converterPreset + "-i \"" + filename + "\" -o \"" + output_file + "\""
-			print(execCommand, file=fp1)
-	fp1.close()
-	os.system("chmod u+x rconvert.sh")
-
+	def GenerateScript(self):
+		fp1 = open('rconvert.sh', 'w')
+		print("#!/bin/bash", file=fp1)
+		print("", file=fp1)
+		for extension in self.SupportedExtensions:
+			filenames = glob.glob('*' + extension)
+			for filename in filenames:
+				input_file_name, input_file_extension = os.path.splitext(filename)
+				output_file = input_file_name + ".mp4"
+				execCommand = self.ConverterName + " " + self.ConverterPreset + " -i \"" + filename + "\" -o \"" + output_file + "\""
+				print(execCommand, file=fp1)
+		fp1.close()
+		os.system("chmod u+x rconvert.sh")
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -49,13 +50,15 @@ if __name__ == '__main__':
 		print(ex)
 		sys.exit(1)
 
+	myRoku = CRoku('HandBrakeCLI','-Z "Very Fast 1080p30"', ['.avi','.mkv','.mov','.webm','.wmv'])
+
 	try:
 		if args.script:
-			GenerateScript()
+			myRoku.GenerateScript()
 			sys.exit(0)
 
 		if args.file:
-			ConvertFile(args.file)
+			myRoku.ConvertFile(args.file)
 			sys.exit(0)
 	except Exception as ex:
 		print(ex)
