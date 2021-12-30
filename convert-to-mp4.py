b39442572ddc904ea89d41d4c9fcbbe6249e41ad
Jim Carr <jim.carr@devtoprd.com>
2021-12-30 14:04:24 -0500

    Refactoring: string interpolation and Python naming conventions

--- convert-to-mp4.py
+++ convert-to-mp4.py
@@ -6,35 +6,35 @@ import os
 import sys
 
 class CConverter:
-	def __init__(self, _converterName, _converterPreset, _supported_extensions):
-		self.ConverterName = _converterName
-		self.ConverterPreset = _converterPreset
-		self.SupportedExtensions = _supported_extensions
+	def __init__(self, _converter_name, _converter_preset, _supported_extensions):
+		self.converter_name = _converter_name
+		self.converter_preset = _converter_preset
+		self.supported_extensions = _supported_extensions
 
-	def ConvertFile(self, input_file, info_only):
+	def convert_file(self, input_file, info_only):
 		# Make sure the file exists.
 		if not os.path.isfile(input_file):
-			raise ValueError(input_file + " not found.")
+			raise ValueError(f"{input_file} not found.")
 
 		# Split out the pieces of the input file name, and build the output file name.
 		input_file_name, input_file_extension = os.path.splitext(input_file)
 		if input_file_extension == ".mp4":
-			output_file = input_file_name + "_new.mp4"
+			output_file = f"{input_file_name}_new.mp4"
 		else:
-			output_file = input_file_name + ".mp4"
+			output_file = f"{input_file_name}.mp4"
 
 		# Is the input file supported as a conversion target?
-		if any(x in input_file_extension for x in self.SupportedExtensions) or info_only:
+		if any(x in input_file_extension for x in self.supported_extensions) or info_only:
 			if info_only:
-				full_command = self.ConverterName + " " + self.ConverterPreset + " --scan -i \"" + input_file + "\""
+				full_command = f"{self.converter_name} {self.converter_preset} --scan -i \"{input_file}\""
 			else:
-				full_command = self.ConverterName + " " + self.ConverterPreset + " -i \"" + input_file + "\" -o \"" + output_file + "\""
+				full_command = f"{self.converter_name} {self.converter_preset} -i \"{input_file}\" -o \"{output_file}\""
 			
 			os.system(full_command)
 		else:
-			raise ValueError(input_file + " is not in a format that can be converted.")	
+			raise ValueError(f"{input_file} is not in a format that can be converted.")	
 
-	def GenerateScript(self, reconvert):
+	def generate_script(self, reconvert):
 		# Open the script file for writing, and write out the crunch-bang line for bash
 		fp1 = open('video-convert.sh', 'w')
 		print("#!/bin/bash", file=fp1)
@@ -42,18 +42,18 @@ class CConverter:
 
 		# For each supported extension, find matching files, then write out a conversion line in this format:
 		# HandBrakeCLI -Z "Very Fast 1080p30" -i "<inputfile>" -o "<outputfile>"
-		for extension in self.SupportedExtensions:
-			filenames = glob.glob('*' + extension)
+		for extension in self.supported_extensions:
+			filenames = glob.glob(f"*{extension}")
 			for filename in filenames:
 				input_file_name, input_file_extension = os.path.splitext(filename)
 				output_file = ""
 				if input_file_extension == ".mp4":
 					if reconvert == True:
-						output_file = input_file_name + "_new.mp4"
+						output_file = f"{input_file_name}_new.mp4"
 				else:
-					output_file = input_file_name + ".mp4"
+					output_file = f"{input_file_name}.mp4"
 				if not output_file == "":
-					execCommand = self.ConverterName + " " + self.ConverterPreset + " -i \"" + filename + "\" -o \"" + output_file + "\""
+					execCommand = f"{self.converter_name} {self.converter_preset} -i \"{filename}\" -o \"{output_file}\""
 					print(execCommand, file=fp1)
 		fp1.close()
 		os.system("chmod u+x video-convert.sh")
@@ -71,19 +71,19 @@ def main(args):
 		print(ex)
 		sys.exit(1)
 
-	myConverter = CConverter('HandBrakeCLI','-Z "Very Fast 1080p30"', ['.3gp','.flv','.avi','.AVI','m4v','.mkv','.mov', '.mp4','.mpg','.ogv','.webm','.wmv'])
+	my_converter = CConverter('HandBrakeCLI','-Z "Very Fast 1080p30"', ['.3gp','.flv','.avi','.AVI','m4v','.mkv','.mov', '.mp4','.mpg','.ogv','.webm','.wmv'])
 
 	try:
 		if args.script:
-			myConverter.GenerateScript(True)
+			my_converter.generate_script(True)
 			sys.exit(0)
 
 		if args.scriptnomp4:
-			myConverter.GenerateScript(False)
+			my_converter.generate_script(False)
 			sys.exit(0)
 
 		if args.file:
-			myConverter.ConvertFile(args.file, args.info)
+			my_converter.convert_file(args.file, args.info)
 			sys.exit(0)
 	except Exception as ex:
 		print(ex)
